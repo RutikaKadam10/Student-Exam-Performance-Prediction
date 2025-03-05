7e81a3731456c9a5447b9c86bc672621b7a93be4
RutikaKadam10 <rutikakadam2727@gmail.com>
2025-03-05 13:49:49 -0500

    Hyperparameter Tuning done

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,8 @@ import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 
+from sklearn.model_selection import GridSearchCV
+
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -19,16 +21,23 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,param):
     try:
         report={}
 
         #list(models) converts dictioary key into list 
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
             #train model
-            model.fit(X_train,y_train)
+            #model.fit(X_train,y_train)
 
             #making prediction on y_train and y_test
             y_train_pred=model.predict(X_train)
@@ -43,4 +52,3 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
 
     except Exception as e:
         raise CustomException(e,sys)
-
